32cd8cb947fbbfee35c701691d9274f00899037d
mtink <mgimnastika23@gmail.com>
2024-09-21 22:47:00 +0200

    6

--- main.cpp
+++ main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 using namespace std;
 
-/// @brief 
-// 1231
-/// @return 
+ 
 
 
 int main() {
@@ -12,22 +10,28 @@ int main() {
     int b = -1;
 
     cout << "A = 10, B = 1, A + B =" << a+b << "\n" << endl;
-
-
-cout    << "Data Type: unsigned int; Size: 4 bytes; min range:: 0; max range:: 4*10^9\n"<< endl;
+    
+    
+    
+    
+    cout    << "Data Type: unsigned int; Size: 4 bytes; min range:: 0; max range:: 4*10^9\n"<< endl;
     unsigned int x = 3;
     unsigned int y = 5;
 
     cout << "X = 3, Y = 5, X * Y =" << x*y << "\n" << endl;
 
-     cout    << "Data Type: long long; Size: 8 bytes; min range: -9*10^18; max range: 9*10^18\n" << endl;
     
+    
+    
+    cout    << "Data Type: long long; Size: 8 bytes; min range: -9*10^18; max range: 9*10^18\n" << endl;
     long long c = 10000000000000000;
     long long d = -1000000000000;
 
     cout << "C = 10000000000000000, D =-1000000000000, C / D =" << c/d << "\n" << endl;
 
 
+    
+    
     cout    << "Data Type: unsigned long long; Size: 8 bytes; min range: 0; max range: 18*10^18\n"<< endl;
 
     unsigned long long s = 1000000000000000000;
@@ -35,8 +39,10 @@ cout    << "Data Type: unsigned int; Size: 4 bytes; min range:: 0; max range:: 4
 
     cout << "S = 1000000000000000000, T = 1000000000000000, S - T =" << s-t << "\n" << endl;
 
+    
+    
+    
     cout    << "Data Type: char; Size: 1 byte; min range: 0; max range:255\n"<< endl;
-
 
     char f = 'A';
     char l = 'B';
@@ -57,8 +63,11 @@ cout    << "Data Type: unsigned int; Size: 4 bytes; min range:: 0; max range:: 4
     double q = 11.111;
     double r = 12.222;
 
-    cout << "Q = 11.111,  R = 12.222, Q + R =" << q+r << endl;
+    cout << "Q = 11.111,  R = 12.222, Q + R =" << q+r << "\n" << endl;
 
+    
+    
+    
     cout    << "Data Type: long; Size: 4 or 8 byte; 4: min range:-2 147 483 648 ; max range:2 147 483 647; 8: min range:-9 223 372 036 854 775 808; max range:+9 223 372 036 854 775 807 \n"<< endl;
 
     long z = 20000000;
@@ -66,6 +75,10 @@ cout    << "Data Type: unsigned int; Size: 4 bytes; min range:: 0; max range:: 4
 
     cout << "Z = 20000000, W =-19999999, Z - W =" << z-w << "\n" << endl;
 
+    
+    
+    
+    
     cout    << "Data Type: short; Size: 2 byte; min range:-32768 ; max range:32767\n"<< endl;
 
     short i = 20000;
@@ -74,7 +87,14 @@ cout    << "Data Type: unsigned int; Size: 4 bytes; min range:: 0; max range:: 4
     cout << "I = 20000, KK =-1999, I - K =" << i+kk << "\n" << endl;
 
 
+    
+    
+    cout    << "Data Type: float; Size: 4 byte; min range:- 3.4E-38; max range: 3.4E+38\n"<< endl;
 
+    float num1 = 1.23E-4;
+    float num2 = 3.65E+6;
+
+    cout << "NUM1 = 1.23E-4, NUM2 = 2.15E+6, NUM1 + NUM2 =" << num1 + num2 << "\n" << endl;
 
     return 0;
 
