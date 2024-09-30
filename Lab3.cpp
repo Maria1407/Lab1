d7dad34c4aac118dfab23859cf721f9b9b8d97f8
mtink <mgimnastika23@gmail.com>
2024-09-30 09:39:45 +0200

    7

--- Lab3.cpp
+++ Lab3.cpp
@@ -2,26 +2,11 @@
 
 using namespace std;
 
-void setBitToZero(int& x, int position) {
-    // Находим маску для установки нужного бита в ноль
-    unsigned int mask = ~(1U << position);
-    
-    // Применяем маску к числу x
-    x &= mask;
-}
 
 int main() {
-    int x = 0b101001010111;
-    int position;
-
-    cout << "Enter position: ";
-    cin >> position;
-
-    cout << "\nx = " << x << endl;
-
-    setBitToZero(x, position);
-
-    cout << "\nX after setting bit to zero = " << x << endl;
-
-    return 0;
+    int number, index; 
+    cin >> number; 
+    cin >> index;
+    number = (number & ~(1 << index));
+    cout << number << endl;
 }
