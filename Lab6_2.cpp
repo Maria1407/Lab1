057dd768ba759e58b1c3e3785fc6acab9a8c225d
Maria <mgimnastika23@gmail.com>
2024-10-21 15:31:16 +0200

    14

--- Lab6_2.cpp
+++ Lab6_2.cpp
@@ -1,51 +1,85 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-int firstDigit(int num) {
-    while (num >= 10) {
-        num /= 10;
-    }
-    return num;
-}
-
-int productOfDigits(int num) {
-    int product = 1;
-    while (num != 0) {
-        int digit = num % 10;
-        product *= digit;
-        num /= 10;
-    }
+#include <iostream> 
+ 
+using namespace std; 
+ 
+int getFirstDigit(int n);
+int productOfDigits(int n); 
+void swap(int &a, int &b);
+ 
+ 
+int main() 
+{    
+    cout << "TASK 2" << endl; 
+ 
+    const int n = 5;  
+    int arr[n];
+  
+    for (int i = 0; i < n; i++) {  
+        cout << "Enter a number: "; 
+        cin >> arr[i]; 
+    }  
+ 
+    for (int i = 0; i < n - 1; ++i)  
+    {  
+        for (int j = i + 1; j < n; ++j)  
+        {  
+            int number1 = arr[i]; 
+            int number2 = arr[j]; 
+ 
+            int first_digit1 = getFirstDigit(arr[i]); 
+            int first_digit2 = getFirstDigit(arr[j]); 
+ 
+            int productOfDigits1 = productOfDigits(arr[i]); 
+            int productOfDigits2 = productOfDigits(arr[j]); 
+             
+            if (first_digit1 > first_digit2)  
+            { 
+                swap(arr[i], arr[j]); 
+            
+            } else if (first_digit1 == first_digit2) { 
+                if (productOfDigits1 > productOfDigits2)  
+                { 
+                swap(arr[i], arr[j]);
+                
+                } else if (productOfDigits1 == productOfDigits2) { 
+                    if (number1 > number2) { 
+                        swap(arr[i], arr[j]); 
+                    } 
+                } 
+            } 
+        } 
+    }  
+     
+    for (int i = 0; i < n; i++)  
+            cout << arr[i] << " "; 
+ 
+    return 0; 
+} 
+ 
+ 
+int productOfDigits(int n)  
+{    
+    int product = 1; 
+    while (n)  
+    { 
+        product *= n % 10;
+        n /= 10;
+    } 
     return product;
-}
-
-bool compareNumbers(int a, int b) {
-    if (firstDigit(a) != firstDigit(b)) {
-        return firstDigit(a) < firstDigit(b);
-    } else if (productOfDigits(a) != productOfDigits(b)) {
-        return productOfDigits(a) < productOfDigits(b);
-    } else {
-        return a < b;
-    }
-}
-
-int main() {
-    int n;
-    cout << "Enter the number of elements: ";
-    cin >> n;
-    
-    int arr[1000];
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    
-    sort(arr, arr + n, compareNumbers);
-    
-    cout << "Sorted sequence: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    
-    return 0;
+} 
+ 
+ 
+int getFirstDigit(int n)  
+{ 
+    while (n >= 10)  
+        n /= 10; 
+     
+    return n;   
+} 
+ 
+void swap(int &a, int &b) { 
+    int temp = a; 
+    a = b; 
+    b = temp; 
+    return; 
 }
