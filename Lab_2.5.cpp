1e145820747f3c7202ebeb4dc206f8c2a942ef67
mtink <mgimnastika23@gmail.com>
2025-03-10 14:24:43 +0200

    30

--- Lab_2.5.cpp
+++ Lab_2.5.cpp
@@ -9,70 +9,17 @@ public:
     MyVector() { // Базовый конструктор
         m_size = 0;
         m_cap = 0;
-        m_arr = nullptr; //nullptr - указатель, который ни на что не указывает
+        m_arr = nullptr;
     }
-
     MyVector(size_t size, int value) { // Конструктор с параметрами
         m_arr = new int[size];
         m_cap = size;
         m_size = size;
     }
 
-    ~MyVector() { //Деструктор
+    ~MyVector() { // Деструктор
         if(m_arr != nullptr)
             delete[] m_arr;
-    }
-
-    void resize(size_t newSize, int value = 0) {
-        if (newSize < m_size) {
-            m_size = newSize;
-        } else {
-            reserve(newSize);
-            for (size_t i = m_size; i < newSize; ++i) {
-                m_arr[i] = value;
-            }
-            m_size = newSize;
-        }
-    }
-
-    void reserve(size_t cap) { // реалакация памяти и новый массив копируем в старые ячейки
-        if(m_size >= cap)
-            return;
-        m_cap = cap; 
-        int* tmp = new int[m_cap];
-        for(int i=0;i<m_size;i++)
-            tmp[i] = m_arr[i];
-        delete[] m_arr;
-        m_arr = tmp;
-    } 
-
-    void shrink_to_fit() { // создание массива меньшего размера
-        m_cap = m_size;
-        int* tmp = new int[m_cap];
-        for(int i=0;i<m_size;i++)
-            tmp[i] = m_arr[i];
-        delete[] m_arr;
-        m_arr = tmp;
-    }
-
-    int& front() {
-        return m_arr[0];
-    }
-
-    int& back() {
-        return m_arr[m_size - 1];
-    }
-
-    size_t size() {
-        return m_size;
-    }
-
-    bool empty() { // пуст?
-        return m_size == 0;
-    }
-
-    size_t capacity() { // емкость
-        return m_cap;
     }
 
     void push_back(int value) {
@@ -89,6 +36,46 @@ public:
         delete[] m_arr;
         m_arr = tmp;
         m_size++;
+    }
+
+    void resize(size_t newSize, int value = 0) {
+        if (newSize < m_size) {
+            m_size = newSize;
+        } else {
+            reserve(newSize);
+            for (size_t i = m_size; i < newSize; ++i) {
+                m_arr[i] = value;
+            }
+            m_size = newSize;
+        }
+    }
+
+    void reserve(size_t cap) {
+        if(m_size >= cap)
+            return;
+        m_cap = cap;
+        int* tmp = new int[m_cap];
+        for(int i=0;i<m_size;i++)
+            tmp[i] = m_arr[i];
+        delete[] m_arr;
+        m_arr = tmp;
+    } 
+
+    void shrink_to_fit() {
+        m_cap = m_size;
+        int* tmp = new int[m_cap];
+        for(int i=0;i<m_size;i++)
+            tmp[i] = m_arr[i];
+        delete[] m_arr;
+        m_arr = tmp;
+    }
+
+    size_t size() { // возвращает текущий размер вектора
+        return m_size;
+    }
+
+    size_t capacity() { // возвращает текущую емкость вектора
+        return m_cap;
     }
 
     void insert(size_t index, int value) {
@@ -112,13 +99,25 @@ public:
         return m_arr[index];
     }
 
-    /*int& at(size_t index) {
+    int& front() {
+        return m_arr[0];
+    }
+
+    int& back() {
+        return m_arr[m_size - 1];
+    }
+
+    bool empty() {
+        return m_size == 0;
+    }
+
+    int& at(size_t index) {
         // Проверка
         return m_arr[index];
-    }*/
+    }
 
-    MyVector(const MyVector&) = delete; // конструктор копирования
-    MyVector& operator=(const MyVector&) = delete; // оператор присваивания
+    MyVector(const MyVector&) = delete;
+    MyVector& operator=(const MyVector&) = delete;
 };
 
 void foo(MyVector vec) {
@@ -126,19 +125,34 @@ void foo(MyVector vec) {
 }
 
 int main() {
-    std::vector<int> mas;
-    mas.resize(20);
-    mas[10] = 2;
-    for(int i=0;i<100;i++) {
-        mas.push_back(i);
-        std::cout << mas.size() << " " << mas.capacity() << std::endl;
-    }
-    mas.shrink_to_fit();
-    std::cout << mas.size() << " " << mas.capacity() << std::endl;
+    try {
+        MyVector mas;
+        for (int i = 0; i < 100; ++i) {
+            mas.push_back(i);
+            std::cout << "MyVector size: " << mas.size() << ", capacity: " << mas.capacity() << std::endl;
+        }
 
-    std::cout << mas[10] << std::endl;
-    mas[10] = 2;
-    std::cout << mas[10] << std::endl;
+        std::cout << "Front element: " << mas.front() << std::endl;
+        std::cout << "Back element: " << mas.back() << std::endl;
+
+        mas.insert(1000, 99);
+        std::cout << "After insert at index 5: " << mas[5] << std::endl;
+
+        mas.erase(5);
+        std::cout << "After erase at index 5: " << mas[5] << std::endl;
+
+        mas.resize(15, 100);
+        std::cout << "After resize to 15: " << mas.size() << ", capacity: " << mas.capacity() << std::endl;
+
+        mas.shrink_to_fit();
+        std::cout << "After shrink_to_fit: " << mas.size() << ", capacity: " << mas.capacity() << std::endl;
+    }
+    catch(const std::out_of_range& e) {
+        std::cerr << "123 " << e.what() << std::endl;
+    }
+    catch(const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
 
     return 0;
 }
